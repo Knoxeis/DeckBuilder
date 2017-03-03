bd717fd601fce87a7a9a6a3d713cafb944453585
Knoxeis <o.pronier@gmail.com>
2017-03-03 19:14:20 +0100

    Image viewer done

--- DeckBuilder-master/app/src/main/java/esiee/hearthstone_deckbuilder/CardAdapter.java
+++ DeckBuilder-master/app/src/main/java/esiee/hearthstone_deckbuilder/CardAdapter.java
@@ -5,18 +5,19 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import com.android.volley.toolbox.NetworkImageView;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
-public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder> {
+public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder> implements Filterable {
 
     private ArrayList<Card> cards;
     private Context context;
+    private ArrayList<Card> orig;
 
     public CardAdapter(Context context, ArrayList<Card> cards) {
         this.cards = cards;
@@ -24,17 +25,15 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder> {
     }
 
     @Override
-    public CardAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cell_layout, viewGroup, false);
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(CardAdapter.ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
         viewHolder.img.setScaleType(NetworkImageView.ScaleType.CENTER_CROP);
         viewHolder.img.setImageUrl(cards.get(i).getImgURL(), VolleyHandler.getInstance().getImageLoader());
-        Picasso.with(context).load(viewHolder.img.getId()).resize(240, 120).into(viewHolder.img);
-
     }
 
     @Override
@@ -42,13 +41,42 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder> {
         return cards.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    class ViewHolder extends RecyclerView.ViewHolder {
         private TextView title;
         private NetworkImageView img;
+
         public ViewHolder(View view) {
             super(view);
-            title = (TextView)view.findViewById(R.id.title);
+            title = (TextView) view.findViewById(R.id.title);
             img = (NetworkImageView) view.findViewById(R.id.img);
         }
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                final FilterResults oReturn = new FilterResults();
+                final ArrayList<Card> results = new ArrayList<Card>();
+                if (orig == null)
+                    orig  = cards;
+                if (constraint != null){
+                    if(orig !=null & orig.size()>0 ){
+                        for ( final Card g :orig) {
+                            if (g.getName().toLowerCase().contains(constraint.toString()))results.add(g);
+                        }
+                    }
+                    oReturn.values = results;
+                }
+                return oReturn;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                cards = (ArrayList<Card>)results.values;
+                notifyDataSetChanged();
+            }
+        };
     }
 }
